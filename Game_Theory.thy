6ab2270552b357d4008d6bb2c922868c3555b3ba
larswe <s1824086@ed.ac.uk>
2022-04-19 00:06:36 +0100

    Opted for players = 0, 1, ..., n-1

--- Game_Theory.thy
+++ Game_Theory.thy
@@ -3,14 +3,18 @@ theory Game_Theory
 begin
 
 locale strategic_game = 
-  fixes number_of_players :: "nat"
+  fixes number_of_players :: "nat" 
+    and strategies :: "nat \<Rightarrow> 'strategy"
 begin
 
 abbreviation "n == number_of_players"
 
-definition players :: "nat set" where "players = {i :: nat . (1 :: nat) \<le> i \<and> i \<le> n}"
+(* Note 0-indexation. *)
+definition players :: "nat set" 
+  where "players = {i . i < n}"
 
-
+lemma n_is_card: "n = card players"
+  by (simp add: players_def) 
 
 
 end
